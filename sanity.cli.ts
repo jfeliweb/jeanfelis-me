74270346a1c498eac5198b2c35ddd2ef9b9d6269
Jean Felisme <jean@clockwork.com>
2025-10-24 13:01:35 -0400

    deploy

---
+++ sanity.cli.ts
@@ -0,0 +1,11 @@
+import { defineCliConfig } from 'sanity/cli';
+
+export default defineCliConfig({
+  deployment: {
+    appId: 'scdw68ty2bg6olbic2iyh1m9',
+  },
+  api: {
+    projectId: 'hasvdp01',
+    dataset: 'production',
+  }
+});
